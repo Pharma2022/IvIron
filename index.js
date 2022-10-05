5781f00fb066037cf1a178eaca37ac334ee9bbd4
Pharma2022 <111680056+Pharma2022@users.noreply.github.com>
2022-10-05 07:06:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ female.addEventListener("click",function(e){
                 
         
         
-  if ((patientHeight>=152&&patientHeight<=200)&&(patientWeight>=25&&patientWeight<=90)&&(patientTargetHb>=110&&patientTargetHb<=150&&patientTargetHb>patientActualHb+10)&&(patientActualHb>=50&&patientActualHb<=150)&&(myIron= "Cosmofer"||"Monofer"||"Ferinject")&&(patientGender=="male"||"female"))      
+  if ((patientHeight>=152&&patientHeight<=200)&&(patientWeight>=25&&patientWeight<=90)&&(patientTargetHb>=110&&patientTargetHb<=150&&patientTargetHb>patientActualHb+10)&&(patientActualHb>=50&&patientActualHb<=150)&&(myIron= "Cosmofer"||"Monofer"||"Ferinject"))      
     {    
         function render(){  renderDose.innerHTML=""
                 renderDose.innerHTML= `
